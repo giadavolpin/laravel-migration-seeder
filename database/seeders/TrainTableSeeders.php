20792aec98c882a68550d42e59a32959b0f0c9f7
giadavolpin <volpingiada@gmail.com>
2022-12-30 17:05:37 +0100

    seeder iniziato

--- database/seeders/TrainTableSeeders.php
+++ database/seeders/TrainTableSeeders.php
@@ -4,9 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Product;
+use App\Models\Train;
 
-class ProductsTableSeeder extends Seeder
+class TrainTableSeeders extends Seeder
 {
     /**
      * Run the database seeds.
@@ -15,7 +15,6 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        $product = config('db_seeder.products');
-        dd ()
+        //
     }
 }
