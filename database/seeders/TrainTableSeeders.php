46433d4da8461d8d9adcbdff0268f7d7782185e8
giadavolpin <volpingiada@gmail.com>
2023-01-01 16:22:50 +0100

    for

--- database/seeders/TrainTableSeeders.php
+++ database/seeders/TrainTableSeeders.php
@@ -18,13 +18,13 @@ class TrainTableSeeders extends Seeder
     {
         for ($i = 0; $i < 10; $i++) {
             $newTrain = new Train();
-            $newTrain->azienda = $faker->word();
-            $newTrain->stazionedipartenza = $faker->city();
-            $newTrain->stazionediarrivo = $faker->city();
-            $newTrain->Orariodipartenza = $faker->time();
-            $newTrain->Orariodiarrivo = $faker->time();
-            $newTrain->Codicetreno = $faker->numberBetween(0, 100);
-            $newTrain->Numerocarrozze = $faker->randomDigit();
+            $newTrain->azienda = $faker->company();
+            $newTrain->stazione_partenza = $faker->city();
+            $newTrain->stazione_arrivo = $faker->city();
+            $newTrain->Orario_partenza = $faker->time();
+            $newTrain->Orario_arrivo = $faker->time();
+            $newTrain->Codice_treno = $faker->numberBetween(0, 100);
+            $newTrain->Numero_carrozze = $faker->randomDigit();
             $newTrain->save();
         }
     }
